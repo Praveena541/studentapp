6e10d4c9b45078dfc59c6685a6752eabb2aef4f9
Avvari <praveena.avvari@accenture.com>
2025-02-05 13:59:03 +0530

    intial commit

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class HeaderComponent extends Component {
+    render() {
+        return(
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+export default HeaderComponent
